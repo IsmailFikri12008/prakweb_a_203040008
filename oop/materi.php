38c35c1d256b52b05870171950088eb844c3e3b8
IsmailFikri12008 <203040008@mail.unpas.ac.id>
2022-10-03 14:05:57 +0700

    menyelesaikan video 1 (pendahuluan oophp)

---
+++ oop/materi.php
@@ -0,0 +1,32 @@
+<!-- 
+
+    Procedural Programming
+- Instruksi dilakukan langkah demi langkah 
+- Memecah program menjadi bagian-bagian kecil 
+- Disebut prosedur, subroutine atau function 
+- Linear / Top-to-Bottom 
+- Fortran, ALGOL, COBOL, Pascal, C, PHP, Javascrip 
+
+    Kelebihan Procedural Programming
+- To-the-point 
+- Simplicity & kemudahan implementasi (untuk compiler & interpreter) 
+- Mudah ditelusuri 
+- Membutuhkan lebih sedikit memory (dibandingkan dengan OOP)
+
+    Object Oriented Programming
+- Menyusun semua kode program dan struktur data sebagai objek 
+- Objek adalah unit dasar dari program 
+- Objek menyimpan data dan perilaku 
+- Objek bisa saling berinteraksi
+- Java, Ruby, Python, C++, Javascript, PHP5
+
+    Kelebihan Object Oriented Programming
+- Representasi dunia nyata 
+- Enkapsulasi & Abstraksi Data 
+- Reusability 
+- Skalabilitas & Ekstensibilitas 
+- Kemudahan pengelolaan 
+- Kolaborasi 
+- Digunakan oleh framework
+
+-->
