28142e18b6c5fb49b53e1fd3a157a11c16d264d3
IsmailFikri12008 <203040008@mail.unpas.ac.id>
2022-10-06 16:40:23 +0700

    menyelesaikan video 1 (pendahuluan mvc)

---
+++ mvc/materi.php
@@ -0,0 +1,23 @@
+<!-- 
+
+    MVC (Model View Controller)
+
+    “Pola arsitektur pada perancangan perangkat lunak berorientasi objek.”
+    “Memisahkan antara tampilan, data dan proses.”
+
+    Kenapa MVC
+    - Organisasi dan Struktur Kode 
+    - Pemisahan logic dan tampilan 
+    - Perawatan Kode 
+    - Implementasi konsep OOP yang sudah dipelajari 
+    - Digunakan oleh banyak Web Application Framework
+
+    Framework MVC
+    Bahasa                      Framework
+    PHP                         CodeIgniter, Laravel, Yii, dll
+    Java                        Spring MVC, JSF, Struts, dll
+    Python                      Django, CherryPy, dll
+    Ruby                        Ruby on Rails, Sinatra ,dll
+    Javascript                  AngularJS, React, Backbone.js, dll   
+
+-->
