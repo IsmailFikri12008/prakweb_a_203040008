01ca17fbc63c82738eb6e0a01434eaa90a6da08a
IsmailFikri12008 <203040008@mail.unpas.ac.id>
2022-10-10 15:19:22 +0700

    menyelesaikan video 11 (delete data)

--- mvc/app/views/mahasiswa/index.php
+++ mvc/app/views/mahasiswa/index.php
@@ -19,9 +19,10 @@
 
       <ul class="list-group">
         <?php foreach ($data['mhs'] as $mhs) : ?>
-        <li class="list-group-item d-flex justify-content-between align-items-start">
+        <li class="list-group-item">
           <?= $mhs['nama']; ?>
-          <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary">Detail</a>
+          <a href="<?= BASEURL; ?>/mahasiswa/hapus/<?= $mhs['id']; ?>" class="badge text-bg-danger float-end m-1" onclick="return confirm('Yakin?');">Hapus</a>
+          <a href="<?= BASEURL; ?>/mahasiswa/detail/<?= $mhs['id']; ?>" class="badge text-bg-primary float-end m-1">Detail</a>
         </li>
         <?php endforeach ?>
       </ul>
